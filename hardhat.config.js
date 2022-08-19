b56c12e1942a0bc722ee9c78d2fab7eeed92a078
kfangw <kfangw@gmail.com>
2022-08-19 11:02:15 +0900

    remove samples unnecessary

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
-require("./tasks/faucet");
+require("./tasks/nft");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
