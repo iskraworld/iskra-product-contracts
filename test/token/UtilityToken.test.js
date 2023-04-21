81776ab9196486f5a58e3faa5012bda30038f302
egon.son <egon.son@iskra.world>
2023-04-21 10:07:50 +0900

    test: add utility token test

---
+++ test/token/UtilityToken.test.js
@@ -0,0 +1,101 @@
+const { ethers } = require("hardhat");
+const {
+  shouldBehaveLikeERC20,
+  shouldBehaveLikeERC20Transfer,
+  shouldBehaveLikeERC20Approve,
+} = require("./ERC20/ERC20.behavior");
+
+describe("UtilityToken contract", function () {
+  const initialSupply = 100000000;
+  let owner, user1, user2;
+
+  beforeEach(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+    const CandyToken = await ethers.getContractFactory("UtilityToken", owner);
+    this.token = await CandyToken.deploy("Candy Token", "CND", owner.address);
+    await this.token.deployed();
+    this.token.mint(owner.address, initialSupply);
+  });
+
+  describe("ERC20 behavior", function () {
+    const initialHolder = {};
+    const recipient = {};
+    const anotherAccount = {};
+
+    beforeEach(async function () {
+      initialHolder.signer = owner;
+      initialHolder.address = owner.address;
+      recipient.signer = user1;
+      recipient.address = user1.address;
+      anotherAccount.signer = user2;
+      anotherAccount.address = user2.address;
+    });
+
+    shouldBehaveLikeERC20(
+      "ERC20",
+      initialSupply,
+      initialHolder,
+      recipient,
+      anotherAccount
+    );
+  });
+
+  describe("ERC20Transfer behavior", function () {
+    const initialHolder = {};
+    const recipient = {};
+
+    beforeEach(async function () {
+      initialHolder.signer = owner;
+      initialHolder.address = owner.address;
+      recipient.signer = user1;
+      recipient.address = user1.address;
+    });
+
+    shouldBehaveLikeERC20Transfer(
+      "ERC20",
+      initialHolder,
+      recipient,
+      initialSupply,
+      function (from, to, value) {
+        return this.token.connect(from.signer).transfer(to, value);
+      }
+    );
+  });
+
+  describe("ERC20Approve behavior", function () {
+    const initialHolder = {};
+    const recipient = {};
+
+    beforeEach(async function () {
+      initialHolder.signer = owner;
+      initialHolder.address = owner.address;
+      recipient.signer = user1;
+      recipient.address = user1.address;
+    });
+
+    shouldBehaveLikeERC20Approve(
+      "ERC20",
+      initialHolder,
+      recipient,
+      initialSupply,
+      function (owner, spender, amount) {
+        return this.token.connect(owner.signer).approve(spender, amount);
+      }
+    );
+  });
+
+  describe("addMinter, removeMinter", function () {
+    it("basic", async function () {
+      await expect(
+        this.token.connect(user1).mint(user1.address, 1000000)
+      ).to.revertedWith("caller is not a minter");
+      await this.token.addMinter(user1.address);
+      await expect(this.token.connect(user1).mint(user1.address, 1000000)).not
+        .to.reverted;
+      await this.token.removeMinter(user1.address);
+      await expect(
+        this.token.connect(user1).mint(user1.address, 1000000)
+      ).to.revertedWith("caller is not a minter");
+    });
+  });
+});
