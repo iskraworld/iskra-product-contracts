1b37a5f179fc6b325c77295d957413b3420baa97
egon.son <egon.son@iskra.world>
2023-04-21 14:00:29 +0900

    fix: remove KIP from GameToken

--- contracts/token/ERC20/GameToken.sol
+++ contracts/token/ERC20/GameToken.sol
@@ -7,29 +7,17 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import "../../kip/IKIP13.sol";
-import "../../kip/IKIP7.sol";
-import "../../kip/IKIP7Metadata.sol";
-import "../../kip/IKIP7Receiver.sol";
 
 // GameToken features
 //  - ERC20 basic functions
 //  - fixed supply
 //  - not mintable, not burnable
 //  - defense for transferring tokens to the token contract itself
-contract GameToken is
-    IKIP13,
-    IKIP7,
-    IKIP7Metadata,
-    IERC165,
-    ERC20,
-    Ownable2Step
-{
+contract GameToken is IERC20Metadata, IERC165, ERC20, Ownable2Step {
     using Address for address;
-
-    bytes4 private constant _KIP7_RECEIVED = 0x9d188c22;
 
     constructor(
         string memory _name,
@@ -42,122 +30,13 @@ contract GameToken is
     function supportsInterface(bytes4 interfaceId)
         external
         pure
-        override(IKIP13, IERC165)
+        override
         returns (bool)
     {
         return
-            interfaceId == type(IKIP13).interfaceId ||
-            interfaceId == type(IKIP7).interfaceId ||
-            interfaceId == type(IKIP7Metadata).interfaceId ||
+            interfaceId == type(IERC20Metadata).interfaceId ||
             interfaceId == type(IERC165).interfaceId ||
             interfaceId == type(IERC20).interfaceId;
-    }
-
-    function name()
-        public
-        view
-        override(IKIP7Metadata, ERC20)
-        returns (string memory)
-    {
-        return super.name();
-    }
-
-    function symbol()
-        public
-        view
-        override(IKIP7Metadata, ERC20)
-        returns (string memory)
-    {
-        return super.symbol();
-    }
-
-    function decimals()
-        public
-        view
-        override(IKIP7Metadata, ERC20)
-        returns (uint8)
-    {
-        return super.decimals();
-    }
-
-    function totalSupply()
-        public
-        view
-        override(IKIP7, ERC20)
-        returns (uint256)
-    {
-        return super.totalSupply();
-    }
-
-    function balanceOf(address account)
-        public
-        view
-        override(IKIP7, ERC20)
-        returns (uint256)
-    {
-        return super.balanceOf(account);
-    }
-
-    function transfer(address recipient, uint256 amount)
-        public
-        override(IKIP7, ERC20)
-        returns (bool)
-    {
-        return super.transfer(recipient, amount);
-    }
-
-    function allowance(address owner, address spender)
-        public
-        view
-        override(IKIP7, ERC20)
-        returns (uint256)
-    {
-        return super.allowance(owner, spender);
-    }
-
-    function approve(address spender, uint256 amount)
-        public
-        override(IKIP7, ERC20)
-        returns (bool)
-    {
-        return super.approve(spender, amount);
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public override(IKIP7, ERC20) returns (bool) {
-        return super.transferFrom(sender, recipient, amount);
-    }
-
-    function safeTransfer(
-        address recipient,
-        uint256 amount,
-        bytes memory data
-    ) external override {
-        _safeTransfer(recipient, amount, data);
-    }
-
-    function safeTransfer(address recipient, uint256 amount) external override {
-        _safeTransfer(recipient, amount, "");
-    }
-
-    function safeTransferFrom(
-        address sender,
-        address recipient,
-        uint256 amount,
-        bytes memory data
-    ) external override {
-        _safeTransferFrom(sender, recipient, amount, data);
-    }
-
-    function safeTransferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external override {
-        _safeTransferFrom(sender, recipient, amount, "");
     }
 
     function _beforeTokenTransfer(
@@ -170,49 +49,5 @@ contract GameToken is
             "cannot transfer tokens to the token contract"
         );
         super._beforeTokenTransfer(from, to, amount);
-    }
-
-    function _safeTransfer(
-        address recipient,
-        uint256 amount,
-        bytes memory data
-    ) internal {
-        transfer(recipient, amount);
-        require(
-            _checkOnKIP7Received(msg.sender, recipient, amount, data),
-            "KIP7: transfer to non KIP7Receiver implementer"
-        );
-    }
-
-    function _safeTransferFrom(
-        address sender,
-        address recipient,
-        uint256 amount,
-        bytes memory data
-    ) internal {
-        transferFrom(sender, recipient, amount);
-        require(
-            _checkOnKIP7Received(sender, recipient, amount, data),
-            "KIP7: transfer to non KIP7Receiver implementer"
-        );
-    }
-
-    function _checkOnKIP7Received(
-        address sender,
-        address recipient,
-        uint256 amount,
-        bytes memory _data
-    ) internal returns (bool) {
-        if (!recipient.isContract()) {
-            return true;
-        }
-
-        bytes4 retval = IKIP7Receiver(recipient).onKIP7Received(
-            msg.sender,
-            sender,
-            amount,
-            _data
-        );
-        return (retval == _KIP7_RECEIVED);
     }
 }
